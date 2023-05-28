34b7967bc9d9f7e0a09cfbeb4b54c7c6d5f27e30
Gurpartap <gurpartap6125@gmail.com>
2023-05-28 21:32:27 +0530

    added the movie tabs to the website

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 const APIURL =
     "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-    const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
-    // we are using the below function to fetch the data from the api url,the below function returns us a promise
-    async function getMovies(){
 
-        const resp= await fetch(APIURL);
-        const respData=await resp.json();
+const main=document.querySelector("main");
 
-        console.log(respData);
+// we are using the below function to fetch the data from the api url,the below function returns us a promise
+async function getMovies() {
 
-        // respData.results.forEach(movie => {
-        //     const img=document.createElement("img")
-        //     img.src=IMGPATH+movie.poster_path;
-        //     document.body.appendChild(img)
-            
-        // });
-        return respData;
+    const resp = await fetch(APIURL);
+    const respData = await resp.json();
+
+    console.log(respData);
+    return respData;
+}
+
+async function show() {
+    const h = await getMovies();
+    h.results.forEach(movie => {
+
+        const {poster_path,title,vote_average}=movie //here we are destructuring these thing else we have to use them as movie.poster_path etc
+        const movieEl = document.createElement("div")
+
+        movieEl.classList.add("movie");
+        movieEl.innerHTML = 
+        `
+            <img src="${IMGPATH+poster_path}" alt="${title}">
+
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+        `
+        main.appendChild(movieEl)
+
+
+    });
+
+}
+show()
+
+function getClassByRate(vote){
+    if(vote>=8){
+        return "green";
     }
-
-   async function show(){
-       const  h= await getMovies();
-        h.results.forEach(movie => {
-            const img=document.createElement("img")
-            img.src=IMGPATH+movie.poster_path;
-            document.body.appendChild(img)
-            
-        });
-
+    else if(vote>=5){
+        return "orange";
     }
-    // getMovies();
-    show()
-    
+    else{
+        return "red";
+    }
+}
